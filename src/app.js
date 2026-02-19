83d14093691c6321d59cd2161de62b3bf809a2ab
Valery <201605998@est.umss.edu>
2026-02-19 11:19:15 -0400

    url/app

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
+// ✅ app.js corregido
 const express = require("express");
 const cors = require("cors");
+const homeRoutes = require("./src/routes/homeRoutes");
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.send("Snapfilm backend running uwu");
-});
+app.use("/api", homeRoutes); // ← rutas aquí, en app.js
 
 module.exports = app;
